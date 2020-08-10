68b767d0f8a3f4c64284c3a2a44728d0575b1cd0
Steven Giesel <stgiesel35@gmail.com>
2020-08-10 21:00:56 +0200

    Empty project structure

---
+++ Converter/LinkDotNet.EnumValueObject.Converter.NHibernateInterceptor/EnumValueObjectInterceptor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using NHibernate;
+
+namespace LinkDotNet.EnumValueObject.Converter.NHibernateInterceptor
+{
+    public class EnumValueObjectInterceptor : EmptyInterceptor
+    {
+    }
+}
