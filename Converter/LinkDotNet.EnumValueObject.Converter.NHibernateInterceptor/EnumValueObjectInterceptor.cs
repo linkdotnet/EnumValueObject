a6725fa4a733bfc067b75ba7e04cec97bf9f0b1b
Steven Giesel <stgiesel35@gmail.com>
2020-08-11 09:16:15 +0200

    Added sample interceptor

--- Converter/LinkDotNet.EnumValueObject.Converter.NHibernateInterceptor/EnumValueObjectInterceptor.cs
+++ Converter/LinkDotNet.EnumValueObject.Converter.NHibernateInterceptor/EnumValueObjectInterceptor.cs
@@ -5,5 +5,33 @@ namespace LinkDotNet.EnumValueObject.Converter.NHibernateInterceptor
 {
     public class EnumValueObjectInterceptor : EmptyInterceptor
     {
+        private ISession _session;
+
+        public override void SetSession(ISession session)
+        {
+            base.SetSession(session);
+            _session = session;
+        }
+
+        public override object Instantiate(string clazz, object id)
+        {
+            var classMetadata = _session.SessionFactory.GetAllClassMetadata()[clazz];
+            var type = classMetadata.MappedClass;
+            if (type.IsSubclassOf(typeof(EnumValueObject<>)))
+            {
+                var result = type.GetMethod("Create").Invoke(null, new[] {id}) as dynamic;
+                if (result.IsFailure)
+                {
+                    throw new ArgumentException($"Could not create {type.FullName} from key {id}");
+                }
+
+                // Call the static Create method
+                return result.Value;
+            }
+
+            return base.Instantiate(clazz, id);
+
+
+        }
     }
 }
